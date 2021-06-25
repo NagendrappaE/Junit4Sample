8a32fcc68871c6710d7a0acd27e06f1602b21159
NagendrappaE-evolvus <nagendrappa.e@evolvussolutions.com>
2021-06-25 11:03:04 +0530

    spring boot with mockito demo

---
+++ boot-restapi-mockito/src/main/java/com/ece/restapi/BootRestapiMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.ece.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestapiMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRestapiMockitoApplication.class, args);
+	}
+
+}
---
+++ boot-restapi-mockito/src/test/java/com/ece/restapi/BootRestapiMockitoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ece.restapi;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class BootRestapiMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
